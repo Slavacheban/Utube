67040b7436588761741e72f0f0e6f0bf6bca6488
Slava <slavacheban22@gmail.com>
2019-04-08 20:16:47 +0300

    Main

--- src/main/java/HW12/Main.java
+++ src/main/java/HW12/Main.java
@@ -5,21 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UtubeParser parser = new UtubeParser();
-        parser.init();
-        List<String> listOfVideoId = new ArrayList<>();
-        listOfVideoId.add("0sWpjUvbGcQ");
-        parser.getCommentsFromVideos(listOfVideoId);
 
-        for (UtubeVideo video : parser.getListOfVideos()) {
-            for (Comment comment : video.getComments()) {
-                System.out.println("Author: " + comment.getNameAutor());
-                System.out.println("Comment: " + comment.getTextComment());
-                System.out.println("Date of last modification: " + comment.getDateOfLastModification());
-                System.out.println("Likes: " + comment.getLikes());
-                System.out.println("");
-            }
-        }
     }
 
 }
