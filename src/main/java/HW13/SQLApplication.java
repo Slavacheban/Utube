d4605c9bdb95f9b3271244b9957f29f81c431797
Slava <slavacheban22@gmail.com>
2019-04-10 13:44:09 +0300

    .sql added

--- src/main/java/HW13/SQLApplication.java
+++ src/main/java/HW13/SQLApplication.java
@@ -1,5 +1,20 @@
 package HW13;
 
+
+import org.apache.log4j.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class SQLApplication {
-    
+    private static final Logger LOGGER = Logger.getLogger(SQLApplication.class);
+    private static final String URL = "jdbc:mysql://localhost:3306/new_schema?serverTimezone=UTC&useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Statement statement = connection.createStatement();
+        connection.close();
+    }
 }
